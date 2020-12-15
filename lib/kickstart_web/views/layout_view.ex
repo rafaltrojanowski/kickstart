b6c7831791edbe991795b74d020acdc5bf017eb4
Rafał Trojanowski <rt.trojanowski@gmail.com>
2020-12-15 18:13:25 +0100

    Add Gravatar image

--- lib/kickstart_web/views/layout_view.ex
+++ lib/kickstart_web/views/layout_view.ex
@@ -1,3 +1,14 @@
 defmodule KickstartWeb.LayoutView do
   use KickstartWeb, :view
+
+  def gravatar(email) do
+    hash = email
+      |> String.trim()
+      |> String.downcase()
+      |> :erlang.md5()
+      |> Base.encode16(case: :lower)
+
+    img = "https://www.gravatar.com/avatar/#{hash}?s=150&d=identicon"
+    img_tag(img, class: "rounded-circle", width: "40", height: "40")
+  end
 end
